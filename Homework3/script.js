8fda7bb0d43216d5195426dca93a73c9d3328578
Ricardo Shaffer <ricardo.ramiro.shaffer@gmail.com>
2020-02-01 10:56:40 -0500

    created framework for js

---
+++ Homework3/script.js
@@ -0,0 +1,13 @@
+
+/* ==== General structure of password code===
+first, establish a password being created.
+2. ask the user to pick a number between 8-128 (parseInt).
+3. check to see if it is a number & if it fits the 8-128.
+4. ask if the user would like upper & lowercase letters (T/F).
+5. ask if the user would like special characters (T/F).
+6. ask if the user would like numbers within their password (T/F)
+7. confirm that at least 4,5,6 steps had a 'true'.
+8. display the result within the document.log on the screen.
+*/
+//Defining the variables
+let assignedPassword;
